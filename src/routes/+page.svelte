5133a0d5fc90365acaa30240fd02287594938c35
KyleTruong <ktruong008@gmail.com>
2023-03-02 10:56:27 -0500

    Add chat input UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,3 +29,26 @@
 
 	<ChatMessage name="Batman" message="Hello Alfred!" />
 </ul>
+
+<form class="mt-4">
+	<div class="relative mt-1 flex items-center">
+		<input
+			type="text"
+			name="search"
+			id="search"
+			class="block w-full rounded-md border-0 py-1.5 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+		/>
+		<button
+			on:click={() => {
+				console.log('clicky');
+			}}
+			type="submit"
+			class="absolute inset-y-0 right-0 flex py-1.5 pr-1.5"
+		>
+			<kbd
+				class="inline-flex items-center rounded border border-gray-200 px-1 font-sans text-xs text-gray-400"
+				>Enter</kbd
+			>
+		</button>
+	</div>
+</form>
